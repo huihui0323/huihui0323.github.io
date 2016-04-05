ef64cdbcb96a437260d5df9ff578adb1e79211d5
huihui0323 <piggydoggy81@gmail.com>
2016-04-06 03:25:13 +0800

    4_6
    change

---
+++ Resources/js/gallery.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('.responsive-img').click(function(e) {
+        $('body').css("overflow", "hidden");
+        else {
+            $('body').css("overflow", "auto!important");
+        }
+    });
+});
+
+
+
